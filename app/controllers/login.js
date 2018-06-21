d9153c0425ca6b2d567064587c35271d6545575a
Logeshwaran Thulasidas <logeshwaran@i-h1-cs-r11-i0250-27.webazilla.com>
2018-06-21 10:42:15 +0500

    completed doctor reg db

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,6 +3,29 @@ import Controller from '@ember/controller';
 export default Controller.extend({
    actions: {
        Login: function() {
+
+        var email=this.get('email');
+        console.log("email :",email);
+
+        var password=this.get('password');
+        console.log("password :",password);
+        $.ajax({
+            type: 'POST',
+            url: "http://localhost:3000/Login",
+            
+            dataType: 'json',
+            data:{email:email,password:password} ,
+            cache: false,
+            success: function(result) {
+
+
+
+    }
+}
+    )
+
+
+
         var category = $('#id_of_select').val(); 
            if(category=="Doctor")
            {
@@ -14,52 +37,9 @@ export default Controller.extend({
            }
            else if(category=="General User"){
             this.transitionToRoute('phome'); 
-           }
-        
-        
-//            alert();
-//            var email = $('#loginEmail').val();
-//            console.log(email);
-//            var password = $('#loginPassword').val();
-//            console.log(password);
-//            var category = $('#id_of_select').val();
-//            console.log(category);
-//            var login = {
-//                "email":email,
-//                "password":password,
-//                "category":category
-//            }
+           } 
            
-           
-//            return $.ajax({
-//            url:'http://localhost:8082/login',
-//            type: 'POST',
-//            contentType: 'application/json',
-//            data: JSON.stringify(login),
-//            success: function(response) {
-//            console.log(JSON.stringify(response));
-//            alert(''+response.message+'');
-
-//                if(category == "doctor"){
-//                    this.transitionToRoute('home');
-// console.log(result)
-//                 }else if(category == "patient"){
-//                     this.transitionToRoute('phome');
-//  console.log(result)
-//                  } else if(category == "lab"){
-//                     this.transitionToRoute('lhome');
-//  console.log(result)
-//                  }
-//                 else
-//                   console.log("Invalid Emailid and Password")
-//                }});
-           
-//            }
-       
-       
-           
-     
-     },
+      },
      Register: function() {
         this.transitionToRoute('reg');
     }}
--- app/controllers/reg.js
+++ app/controllers/reg.js
@@ -1,6 +1,86 @@
 import Controller from '@ember/controller';
 
-export default Controller.extend({
-});
- 
-						 
+
+
+export default Controller.extend( {
+    isShowingModal: false,
+    showLogin: true,
+    ShowRequest: false, //this is to show request id
+   
+    actions: {
+        DocSubmit: function() {
+            console.log("115")
+            var email = this.get('email');
+            console.log(email);
+            var password = this.get('password');
+            console.log(password);
+            var confirmpassword = this.get('confirmpassword');
+            console.log(confirmpassword);
+            var doctorname = this.get('doctorname');
+            console.log(doctorname);
+            var lastname = this.get('lastname');
+            console.log(lastname);
+            var dob = this.get('dob');
+            console.log(dob);
+            var experience = this.get('experience');
+            console.log(experience);
+            var designation= this.get('designation');
+            console.log(designation);
+            var phonenumber = this.get('phonenumber');
+            console.log(phonenumber);
+            var hospitalname= this.get('hospitalname');
+            console.log(hospitalname);
+            var qualification = this.get('qualification');
+            console.log(qualification);
+            var address = this.get('address');
+            console.log(address);
+            var licenseid = this.get('licenseid');
+            console.log(licenseid);
+
+            var mycontroller = this;
+            var dataString = {
+                "email": email,
+                "password": password,
+                "confirmpassword":confirmpassword,
+                "doctorname":doctorname,
+                "lastname":lastname,
+                "dob":dob,
+                "experience":experience,
+                "designation":designation,
+                "qualification":qualification,
+                "address":address,
+                "licenseid":licenseid,
+                "hospitalname":hospitalname,
+                "phonenumber":phonenumber
+
+
+
+                //    "usertype": usertype,
+            };
+            console.log(JSON.stringify(dataString));
+                console.log(email);
+                console.log(password);
+                return $.ajax({
+                    url: 'http://localhost:8082/registerFront',
+                    type: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify(dataString),
+                    success: function(response) {
+                        console.log((response));
+                        var message = response.message
+                        console.log("message", message)
+                        if (message === "Registration` Successful") {
+                            mycontroller.set('message', message)
+                            
+                            var status = response.status
+                            mycontroller.set('usertype', usertype)
+                            console.log(usertype)
+                        } else {
+                            console.log("else")
+                        }
+
+                    }
+                })
+            } 
+    },
+    });
